2023cf3ba792f9f887caef75e0c9b883f9d53c64
Harsh Patel <61933209+Harshpatel2222@users.noreply.github.com>
2021-06-03 13:43:04 +0530

    new customer added

--- Banking_System/bank/urls.py
+++ Banking_System/bank/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home_view, name='index'), 
+    path('new_customer/',views.new_customer, name='new_customer'), 
 ]
--- Banking_System/bank/views.py
+++ Banking_System/bank/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+from .models import *
+from django.contrib import messages
 
 def home_view(request):
     context={}
     return render(request,'index.html',context)
+
+def new_customer(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        number = request.POST.get('number')
+        email = request.POST.get('email')
+        current_balance = request.POST.get('current_balance')
+        new_customer = NewCustomer(name=name, number=number, email=email, current_balance=current_balance)
+        new_customer.save()
+        messages.success(request, 'Your form has been submitted successfully!')
+
+    return render(request, "new_customer.html")
+    
