b0d43e53d9e6afa62960995965366634beb92220
Harsh Patel <61933209+Harshpatel2222@users.noreply.github.com>
2021-06-06 17:25:47 +0530

    updated settings

--- Banking_System/settings.py
+++ Banking_System/settings.py
@@ -118,14 +118,18 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/images/'
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
